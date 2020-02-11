5521bfd05a0d6e4f9712f914df294e50f94932b8
Yasen <3622krim@gmail.com>
2020-02-10 16:56:04 -0800

    Updates

---
+++ precache-manifest.eccf006fa24c494c5440230f9eb296f3.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "675069449442745e23130adca1317e19",
+    "url": "/c_pointer/index.html"
+  },
+  {
+    "revision": "af9db2287bd7a226453b",
+    "url": "/c_pointer/static/css/2.28dd4849.chunk.css"
+  },
+  {
+    "revision": "af9db2287bd7a226453b",
+    "url": "/c_pointer/static/js/2.ed636ce2.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/c_pointer/static/js/2.ed636ce2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3e351d61fe9b9098da04",
+    "url": "/c_pointer/static/js/main.8e8b659f.chunk.js"
+  },
+  {
+    "revision": "58a40b8fc626d6319ede",
+    "url": "/c_pointer/static/js/runtime-main.52aea235.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/c_pointer/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/c_pointer/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/c_pointer/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/c_pointer/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/c_pointer/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
